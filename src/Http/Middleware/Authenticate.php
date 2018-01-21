e1ea740904738c18f96c52462657cf4a0b45c26d
z-song <zosong@126.com>
2018-01-21 17:08:29 +0800

    fix

--- src/Http/Middleware/Authenticate.php
+++ src/Http/Middleware/Authenticate.php
@@ -6,6 +6,13 @@ use Encore\RedisManager\RedisManager;
 
 class Authenticate
 {
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return \Illuminate\Http\Response|null
+     */
     public function handle($request, $next)
     {
         return RedisManager::check($request) ? $next($request) : abort(403);
